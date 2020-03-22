b314d3481b16697e6a4eccb5a0b6b26b2ca96dad
Rhuan Abbud <rhuan.abbud@hotmail.com>
2020-03-22 14:47:29 -0300

    projeto criado

---
+++ src/main/java/br/com/rhuan/cursorhuan/CursorhuanApplication.java
@@ -0,0 +1,13 @@
+package br.com.rhuan.cursorhuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorhuanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorhuanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rhuan/cursorhuan/CursorhuanApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rhuan.cursorhuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorhuanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
